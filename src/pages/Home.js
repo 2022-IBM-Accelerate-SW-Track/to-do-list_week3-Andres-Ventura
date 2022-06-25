009efd3417b2d391fbfcb14f997fcac2ea3832a3
Andy Ventura <andyventura@Andys-iMac.home>
2022-06-24 23:04:52 -0700

    Week 3 complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,7 +29,9 @@ class Home extends Component {
   // updates the state with the new list.
   addTodo = (todo) => {
     const exists = this.state.todos.find(t => t.content === todo.content);
-    if (exists){ return }
+    const due = todo.due;
+    if (exists){return}
+    if((due === "Invalid Date") || (due === null)) { return }
     // In React, keys or ids in a list help identify which items have changed, been added or removed. Keys
     // should not share duplicate values.
     // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
@@ -42,7 +44,8 @@ class Home extends Component {
     this.setState({
       todos: new_list,
     });
-  };
+}
+ 
   render() {
     return (
       <div className="Home">
